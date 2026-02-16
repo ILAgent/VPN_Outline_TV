ff132052e611e8ed3c2270bc79102a833372ed7c
IL_Agent <rvverh@mail.ru>
2026-02-16 18:01:41 +0300

    enable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
             applicationIdSuffix = ".debug"
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
